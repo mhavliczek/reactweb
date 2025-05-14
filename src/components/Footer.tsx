5d60aef4b8cc2de088ab22337e85b06f2cba5024
mhavliczek <mhavliczek@gmail.com>
2025-05-14 00:26:57 -0400

    Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer: React.FC = () => {
   return (
     <footer className="text-center text-gray-400 text-sm py-8 border-t border-gray-200 mt-12">
-      <p>Desarrollado por los estudiantes de Ingeniería Civil Industrial – Universidad 2025</p>
+      <p>Desarrollado por Estudiantes Grupo 9 de Ingeniería Civil Industrial – UDLA 2025</p>
      
     </footer>
   );
